d024e72e482af1da19c06f5bcc1a1d6b7d6a07a7
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-26 18:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-window.promises = [];
+ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
  
-  let a = new promise(resolve => {
+let a = new promise(resolve => {
 	  setTimeout(() => {
-		  resolved("a")
+		  resolve("a")
 	  },3000)
   })
 let b= new Promise(resolve => {
 	  setTimeout(() => {
-		  resolved("b")
+		  resolve("b")
 	  },2000)
   })
 let c= new Promise(resolve => {
 	  setTimeout(() => {
-		  resolved("c")
+		  resolve("c")
 	  },1000)
   })
 let d= new Promise(resolve => {
 	  setTimeout(() => {
-		  resolved("d")
+		  resolve("d")
 	  },4000)
   })
 let e= new Promise(resolve => {
 	  setTimeout(() => {
-		  resolved("e")
+		  resolve("e")
 	  },5000)
   })
 
