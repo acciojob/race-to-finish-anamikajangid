7a470cafbf72dca93a2735df3db6796d063e9433
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-06-26 19:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,32 @@
- 
+let a = new Promise(resolve => {
+    setTimeout(() => {
+        resolve("a")
+    }, 3000)
+});
+let b = new Promise(resolve => {
+    setTimeout(() => {
+        resolve("b")
+    }, 2000)
+});
+let c = new Promise(resolve => {
+    setTimeout(() => {
+        resolve("c")
+    }, 1000)
+});
+let d = new Promise(resolve => {
+    setTimeout(() => {
+        resolve("d")
+    }, 4000)
+});
+let e = new Promise(resolve => {
+    setTimeout(() => {
+        resolve("e")
+    }, 5000)
+});
 
-// Do not change the code above this
-// add your promises to the array `promises`
+window.promises = [a, b, c, d, e];
 
-
-let a = new promise(resolve => {
-	  setTimeout(() => {
-		  resolve("a")
-	  },3000)
-  })
-let b= new Promise(resolve => {
-	  setTimeout(() => {
-		  resolve("b")
-	  },2000)
-  })
-let c= new Promise(resolve => {
-	  setTimeout(() => {
-		  resolve("c")
-	  },1000)
-  })
-let d= new Promise(resolve => {
-	  setTimeout(() => {
-		  resolve("d")
-	  },4000)
-  })
-let e= new Promise(resolve => {
-	  setTimeout(() => {
-		  resolve("e")
-	  },5000)
-  })
-window.promises = [a,b,c,d,e];
-Promise.any(promises).then(result => { const  element = document.getElementById("output")
-	element.innerText = result; });
-
-
-// let x = promise.any([a, b, c, d, e])
-// x.then((data) => {
-// 	const  element = document.getElementById("output")
-// 	element.innerText = data;
-
-
-
-	
-})
+Promise.any(promises).then(result => {
+    const element = document.getElementById("output");
+    element.innerText = result;
+});
